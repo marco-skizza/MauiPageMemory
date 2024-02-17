25eca5b0b6a9684cf1ab4ed7211fa9aa1b1a7dd9
Marco Finschi <Marco.Finschi@lu.ch>
2024-02-17 15:06:37 +0100

    Starter MAUI .NET 8 project

---
+++ MauiPageMemory/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiPageMemory
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
